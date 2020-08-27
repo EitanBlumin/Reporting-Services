d1e8064b76ed715d9d3feddaf37e7199865db80a
Eitan Blumin <eitan@madeiradata.com>
2020-08-27 10:10:16 +0300

    Various improvements

--- CustomSecuritySample/Configure.ps1
+++ CustomSecuritySample/Configure.ps1
@@ -1,63 +1,88 @@
 ﻿param(
-	[string]$UserAccountsSqlInstance = ".",
+	[string]$UserAccountsSqlInstance = "localhost",
 	[string]$UserAccountsDatabaseName = "ReportServerAccounts",
+	[string]$UserAccountsUsername = "",
+	[string]$UserAccountsPassword = "",
     [string]$SSRSAdminUsername = "SSRSAdmin",
     [string]$SSRSWebUsername = "WebUser",
-    [string]$SSRSHost = "http://localhost:80",
-	[string]$SSRSInstallPath = "C:\Program Files\Microsoft Report Server\SSRS"
+    [string]$SSRSHostReportServer = "http://localhost:80/ReportServer",
+	[string]$SSRSInstallPath = "C:\Program Files\Microsoft SQL Server Reporting Services\SSRS"
 	#[string]$SSRSInstallPath = "C:\Program Files\Microsoft Power BI Report Server\PBIRS"
 	#[string]$SSRSInstallPath = "C:\Program Files\Microsoft SQL Server\MSRS13.MSSQLSERVER\Reporting Services"
 	#[string]$SSRSInstallPath = "C:\Program Files\Microsoft SQL Server\MSRS12.MSSQLSERVER\Reporting Services"
+    ,[switch]$DoNotCreateMachineKeys
+    #TODO: ,[switch]$EnableCORS
 )
+
+$ScriptStartTime = "yyyyMMdd_HHmmss" -f (Get-Date)
 
 Write-Verbose "UserAccountsSqlInstance: $UserAccountsSqlInstance"
 Write-Verbose "UserAccountsDatabaseName: $UserAccountsDatabaseName"
 Write-Verbose "SSRSAdminUsername: $SSRSAdminUsername"
 Write-Verbose "SSRSWebUsername: $SSRSWebUsername"
-Write-Verbose "SSRSHost: $SSRSHost"
+Write-Verbose "SSRSHostReportServer: $SSRSHostReportServer"
 Write-Verbose "SSRSInstallPath: $SSRSInstallPath"
+Write-Verbose "DoNotCreateMachineKeys: $DoNotCreateMachineKeys"
 
 if (-not (Test-Path "$SSRSInstallPath"))
 {
 	Write-Error "Path not found: $SSRSInstallPath"
-	Exit
+	Pause
 }
 
+if ($UserAccountsUsername -ne $null -and $UserAccountsUsername -ne "" -and $UserAccountsPassword -ne $null)
+{
+    $dbConnString = "Server=$UserAccountsSqlInstance;User Id=$UserAccountsUsername;Password=$UserAccountsPassword"
+}
+else
+{
+    $dbConnString = "Server=$UserAccountsSqlInstance;Integrated Security=SSPI"
+}
+
+Write-Host "Creating the User Store Database `n" -ForegroundColor Green
+Invoke-SqlCmd -ConnectionString $dbConnString -InputFile "Setup\CreateUserStore.Sql" -Variable @("DBName=$UserAccountsDatabaseName")
+
+$dbConnString = $dbConnString + ";database=$UserAccountsDatabaseName"
+Write-Verbose "UserAccountsConnectionString: $dbConnString"
+
 $appConfigFilePath = "Microsoft.Samples.ReportingServices.CustomSecurity.dll.config"
-Write-Host "Updating $appConfigFilePath `n"
+
+Write-Host "Updating $appConfigFilePath `n" -ForegroundColor Green
 [xml]$appConfigFile = (Get-Content $appConfigFilePath)
 $appConfigFile.configuration.applicationSettings.'Microsoft.Samples.ReportingServices.CustomSecurity.Properties.Settings'.setting|where {$_.name -eq "AnonymousUser"} | ForEach-Object { $_.value = $SSRSWebUsername }
-$appConfigFile.configuration.applicationSettings.'Microsoft.Samples.ReportingServices.CustomSecurity.Properties.Settings'.setting|where {$_.name -eq "Microsoft_Samples_ReportingServices_CustomSecurity_localhost_ReportingService2010"} | ForEach-Object { $_.value = "$SSRSHost/ReportServer/ReportService2010.asmx" }
-$appConfigFile.configuration.applicationSettings.'Microsoft.Samples.ReportingServices.CustomSecurity.Properties.Settings'.setting|where {$_.name -eq "Database_ConnectionString"} | ForEach-Object { $_.value = "Server=$UserAccountsSqlInstance;Integrated Security=SSPI;database=$UserAccountsDatabaseName" }
+$appConfigFile.configuration.applicationSettings.'Microsoft.Samples.ReportingServices.CustomSecurity.Properties.Settings'.setting|where {$_.name -eq "Microsoft_Samples_ReportingServices_CustomSecurity_localhost_ReportingService2010"} | ForEach-Object { $_.value = "$SSRSHostReportServer/ReportService2010.asmx" }
+$appConfigFile.configuration.applicationSettings.'Microsoft.Samples.ReportingServices.CustomSecurity.Properties.Settings'.setting|where {$_.name -eq "Database_ConnectionString"} | ForEach-Object { $_.value = $dbConnString }
 $appConfigFile.Save($appConfigFilePath)
-
-$SetupScriptParameters = @("DBName=$UserAccountsDatabaseName")
-Write-Host "Creating the User Store Database"
-Invoke-SqlCmd -S "." -i "Setup\CreateUserStore.Sql" -Variable $SetupScriptParameters
 
 Write-Host "Copying Logon.aspx page `n" -ForegroundColor Green
 Copy-Item -Path Logon.aspx -Destination "$SSRSInstallPath\ReportServer\"
 
+if (Test-Path "Logon.aspx.cs") {
+    Write-Host "Copying Logon.aspx.cs page `n" -ForegroundColor Green
+    Copy-Item -Path Logon.aspx.cs -Destination "$SSRSInstallPath\ReportServer\"
+}
+
 Write-Host "Copying Microsoft.Samples.ReportingServices.CustomSecurity.dll `n" -ForegroundColor Green
 Copy-Item -Path Microsoft.Samples.ReportingServices.CustomSecurity.dll -Destination "$SSRSInstallPath\ReportServer\Bin\"
 Copy-Item -Path Microsoft.Samples.ReportingServices.CustomSecurity.dll -Destination "$SSRSInstallPath\Portal\"
-if (Test-Path "$SSRSInstallPath\PowerBi") Copy-Item -Path Microsoft.Samples.ReportingServices.CustomSecurity.dll -Destination "$SSRSInstallPath\PowerBi"
+if (Test-Path "$SSRSInstallPath\PowerBi") { Copy-Item -Path Microsoft.Samples.ReportingServices.CustomSecurity.dll -Destination "$SSRSInstallPath\PowerBi" }
 
 Write-Host "Copying $appConfigFilePath `n" -ForegroundColor Green
 Copy-Item -Path $appConfigFilePath -Destination "$SSRSInstallPath\ReportServer\Bin\"
 Copy-Item -Path $appConfigFilePath -Destination "$SSRSInstallPath\Portal\"
-if (Test-Path "$SSRSInstallPath\PowerBi") Copy-Item -Path $appConfigFilePath -Destination "$SSRSInstallPath\PowerBi"
+if (Test-Path "$SSRSInstallPath\PowerBi") { Copy-Item -Path $appConfigFilePath -Destination "$SSRSInstallPath\PowerBi" }
 
 Write-Host "Copying Microsoft.Samples.ReportingServices.CustomSecurity.pdb `n" -ForegroundColor Green
 Copy-Item -Path Microsoft.Samples.ReportingServices.CustomSecurity.pdb -Destination "$SSRSInstallPath\ReportServer\Bin\"
 Copy-Item -Path Microsoft.Samples.ReportingServices.CustomSecurity.pdb -Destination "$SSRSInstallPath\Portal\"
-if (Test-Path "$SSRSInstallPath\PowerBi") Copy-Item -Path Microsoft.Samples.ReportingServices.CustomSecurity.pdb -Destination "$SSRSInstallPath\PowerBi"
+if (Test-Path "$SSRSInstallPath\PowerBi") { Copy-Item -Path Microsoft.Samples.ReportingServices.CustomSecurity.pdb -Destination "$SSRSInstallPath\PowerBi" }
 
 Write-Host "Updating rsreportserver.config `n" -ForegroundColor Green
 $rsConfigFilePath = "$SSRSInstallPath\ReportServer\rsreportserver.config"
 [xml]$rsConfigFile = (Get-Content $rsConfigFilePath)
-Write-Host "Copy of the original config file in $rsConfigFilePath.backup"
-$rsConfigFile.Save("$rsConfigFilePath.backup")
+
+Write-Host "Copy of the original config file in:`n $rsConfigFilePath.$ScriptStartTime.backup"
+$rsConfigFile.Save("$rsConfigFilePath.$ScriptStartTime.backup")
 $rsConfigFile.Configuration.Authentication.AuthenticationTypes.InnerXml = "<Custom />"
 
 $extension = $rsConfigFile.CreateElement("Extension")
@@ -75,8 +100,9 @@ $rsConfigFile.Save($rsConfigFilePath)
 Write-Host "Updating RSSrvPolicy.config `n" -ForegroundColor Green
 $rsPolicyFilePath = "$SSRSInstallPath\ReportServer\rssrvpolicy.config"
 [xml]$rsPolicy = (Get-Content $rsPolicyFilePath)
-Write-Host "Copy of the original config file in $rsPolicyFilePath.backup"
-$rsPolicy.Save("$rsPolicyFilePath.backup")
+
+Write-Host "Copy of the original config file in:`n $rsPolicyFilePath.$ScriptStartTime.backup"
+$rsPolicy.Save("$rsPolicyFilePath.$ScriptStartTime.backup")
 
 $codeGroup = $rsPolicy.CreateElement("CodeGroup")
 $codeGroup.SetAttribute("class","UnionCodeGroup")
@@ -92,8 +118,9 @@ $rsPolicy.Save($rsPolicyFilePath)
 Write-Host "Updating web.config `n" -ForegroundColor Green
 $webConfigFilePath = "$SSRSInstallPath\ReportServer\web.config"
 [xml]$webConfig = (Get-Content $webConfigFilePath)
-Write-Host "Copy of the original config file in $webConfigFilePath.backup"
-$webConfig.Save("$webConfigFilePath.backup")
+
+Write-Host "Copy of the original config file in:`n $webConfigFilePath.$ScriptStartTime.backup"
+$webConfig.Save("$webConfigFilePath.$ScriptStartTime.backup")
 $webConfig.configuration.'system.web'.identity.impersonate="false"
 $webConfig.configuration.'system.web'.authentication.mode="Forms"
 $webConfig.configuration.'system.web'.authentication.InnerXml="<forms loginUrl=""logon.aspx"" name=""sqlAuthCookie"" timeout=""60"" path=""/""></forms>"
@@ -151,16 +178,18 @@ $validationAlgorithm = 'SHA1'
 #}
 #Generate-MachineKey -validation sha1
 
-Write-Host "Adding Machine Keys to $rsConfigFilePath `n" -ForegroundColor Green
-[xml]$rsConfigFile = (Get-Content $rsConfigFilePath)
-$machineKey = $rsConfigFile.CreateElement("MachineKey")
-$machineKey.SetAttribute("ValidationKey", $decryptionKey)
-$machineKey.SetAttribute("DecryptionKey", $validationKey)
-$machineKey.SetAttribute("Validation", $validationAlgorithm)
-$machineKey.SetAttribute("Decryption", $decryptionAlgorithm)
-$rsConfigFile.Configuration.AppendChild($machineKey)
-$rsConfigFile.Save($rsConfigFilePath)
-
+if (-not $DoNotCreateMachineKeys)
+{
+    Write-Host "Adding Machine Keys to $rsConfigFilePath `n" -ForegroundColor Green
+    [xml]$rsConfigFile = (Get-Content $rsConfigFilePath)
+    $machineKey = $rsConfigFile.CreateElement("MachineKey")
+    $machineKey.SetAttribute("ValidationKey", $decryptionKey)
+    $machineKey.SetAttribute("DecryptionKey", $validationKey)
+    $machineKey.SetAttribute("Validation", $validationAlgorithm.ToUpperInvariant())
+    $machineKey.SetAttribute("Decryption", $decryptionAlgorithm.ToUpperInvariant())
+    $rsConfigFile.Configuration.AppendChild($machineKey)
+    $rsConfigFile.Save($rsConfigFilePath)
+}
 
 Write-Host "Configuring Passthrough cookies `n" -ForegroundColor Green
 [xml]$rsConfigFile = (Get-Content $rsConfigFilePath)
